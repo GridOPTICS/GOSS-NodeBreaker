84042a8c4e41e64105658958a396f9dac2d4be55
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-10-30 18:52:11 -0700

    Added tests for topological nodes comparisons.

---
+++ rdf-import-server/src/test/java/pnnl/goss/specs/TestTopologicalNode.groovy
@@ -0,0 +1,70 @@
+package pnnl.goss.specs
+import pnnl.goss.rdf.impl.ConnectivityNode
+import pnnl.goss.rdf.impl.TopologicalNode
+import spock.lang.Specification;
+
+class TestTopologicalNode extends Specification {
+		
+	def "When two instances of topological nodes are created, they are eequivelent"() {
+		
+		given: "The creation of two topological nodes with no attributes defined."
+		
+		TopologicalNode t1 = new TopologicalNode()
+		TopologicalNode t2 = new TopologicalNode()
+		
+		expect: "The two should equal each other"
+		assert t1 == t2
+	} 
+	
+	def "When two instances are changed by identifier"() {
+		given: "Two instances with different identifiers"
+		
+		TopologicalNode t1 = new TopologicalNode("identifier": "F1")
+		TopologicalNode t2 = new TopologicalNode("identifier": "F2")
+		
+		expect: "The two aren't equal"
+		assert t1 != t2
+	}
+	
+	def "When two instances that have the same identifier and the same collections" (){
+		given: "Two topoloogy nodes with the same connectivity nodes are equivelent."
+		
+		ConnectivityNode node = new ConnectivityNode('mrid': 'dadfadfe');
+		def nodes = [new ConnectivityNode('mrid': 'dadfadfe'), new ConnectivityNode('mrid': 'dadfa'), new ConnectivityNode('mrid': 'fadfe')]
+		
+		TopologicalNode t1 = new TopologicalNode()
+		TopologicalNode t2 = new TopologicalNode()
+		
+		assert t1.connectivityNodes.size() == 0
+		assert node != null
+		
+		t1.connectivityNodes += nodes
+		t2.connectivityNodes += nodes
+		
+		expect: "The two topological nodes are equal"
+		assert t1.connectivityNodes == t2.connectivityNodes
+		assert t1 == t2 		
+	}
+	
+	def "When two instances that have the same identifier and a different collections" (){
+		given: "Two topoloogy nodes with differing connectivity nodes are not equivelant."
+		
+		ConnectivityNode node = new ConnectivityNode('mrid': 'dadfadfe');
+		def nodes = [new ConnectivityNode('mrid': 'dadfadfe'), new ConnectivityNode('mrid': 'dadfa'), new ConnectivityNode('mrid': 'fadfe')]
+		def othernodes = [new ConnectivityNode('mrid': 'dad'), new ConnectivityNode('mrid': 'dadfa'), new ConnectivityNode('mrid': 'fadfe')]
+		
+		TopologicalNode t1 = new TopologicalNode()
+		TopologicalNode t2 = new TopologicalNode()
+		
+		assert t1.connectivityNodes.size() == 0
+		assert node != null
+		
+		t1.connectivityNodes += nodes
+		t2.connectivityNodes += othernodes
+		
+		expect: "The two topological nodes are equal"
+		assert t1.connectivityNodes != t2.connectivityNodes
+		assert t1 != t2
+	}
+
+}
