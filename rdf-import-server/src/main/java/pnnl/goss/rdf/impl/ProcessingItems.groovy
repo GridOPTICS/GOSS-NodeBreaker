59bc038df203ef33afca201581e33b75b83ab25d
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-10-31 16:29:52 -0700

    Updated test to add mutiple items.

--- rdf-import-server/src/main/java/pnnl/goss/rdf/impl/ProcessingItems.groovy
+++ rdf-import-server/src/main/java/pnnl/goss/rdf/impl/ProcessingItems.groovy
@@ -5,10 +5,30 @@ import java.util.HashMap;
 
 import pnnl.goss.rdf.EscaType;
 
+/**
+ * ProcessingItems includes a container of items that are to be processed.  It
+ * manages the processing state of the individual items.
+ * 
+ * @author Craig Allwardt
+ *
+ */
 class ProcessingItems {
 	
+	/**
+	 * Keep a record of what has/hasn't been processed.
+	 */
 	Map<EscaType, Boolean> record = new HashMap<EscaType, Boolean>();
 	
+	def areAllProcessed(){
+		// if all are processed then all should not be false			
+		return record.values().find({it == false}) == null
+	}
+	
+	def addItemsToProcess(List<EscaType> items){
+		items.each {addItemToProcess(it)}
+	}
+	
+	// Add an item to the processable elemnts.
 	def addItemToProcess(EscaType item){
 		if (!record.containsKey(item)){
 			record.put(item, false)
--- rdf-import-server/src/test/java/pnnl/goss/specs/TestProcessingItems.groovy
+++ rdf-import-server/src/test/java/pnnl/goss/specs/TestProcessingItems.groovy
@@ -17,14 +17,39 @@ class TestProcessingItems  extends Specification{
 		
 		then: 'item processed to be false'
 		assert processingItems.wasProcessed(item) == false
+		and: "all items haven't been processed"
+		assert processingItems.areAllProcessed() == false
 		
 		when: 'Processing item'
 		processingItems.processItem(item)
 		
 		then: 'Was processed should be true'
 		assert processingItems.wasProcessed(item) == true 
+		and: 'All elements should have been processed'
+		assert processingItems.areAllProcessed()		
+	}
+	
+	def "Adding multiple itesm to processing items"(){
+		ProcessingItems processingItems = new ProcessingItems();
 		
+		given: "Given items to be processed"
+		def items = [DefaultEscaType.construct(null, 'Terminal', 'm1'),
+			DefaultEscaType.construct(null, 'Terminal', 'm2'),
+			DefaultEscaType.construct(null, 'Terminal', 'm3')]
 		
+		when: 'Adding three items'
+		processingItems.addItemsToProcess(items)
+		then: 'areallprocessed is false'
+		assert processingItems.areAllProcessed() == false
+		when: 'processing an item'
+		processingItems.processItem(items.pop())
+		then: 'still should have items to processs'
+		assert processingItems.areAllProcessed() == false
+		when: 'processing next two items'
+		processingItems.processItem(items.pop())
+		processingItems.processItem(items.pop())
+		then: 'All items should be processed'
+		processingItems.areAllProcessed() == true
 		
 		
 	}
