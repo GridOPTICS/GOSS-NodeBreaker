ca79530b501afc399c49ff16ecfc11b38e4addd8
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-10-31 23:58:03 -0700

    Add an initialize funcction to populate the different elements of the topological node from the connectivity nodes.

--- rdf-import-server/src/main/java/pnnl/goss/rdf/impl/TopologicalNode.groovy
+++ rdf-import-server/src/main/java/pnnl/goss/rdf/impl/TopologicalNode.groovy
@@ -1,26 +1,125 @@
 package pnnl.goss.rdf.impl
 
+import java.awt.event.ItemEvent;
+
 import com.hp.hpl.jena.rdf.model.Resource
+
 import pnnl.goss.rdf.EscaType;
+import pnnl.goss.rdf.server.EscaVocab
 import groovy.transform.EqualsAndHashCode
 
 @EqualsAndHashCode
 class TopologicalNode {
 	
 	EscaType substation;
-	EscaType voltageLevel;
+	boolean hasBeenInitialized = false;
+	protected void setHasBeenInitialized(v) {this.hasBeenInitialized = v}
 	
+	double baseVoltage;
+	protected void setBaseVoltage(v){this.baseVoltage = v}
+	
+	EscaType baseVoltageEsca;
+	protected void setBaseVoltageEsca(v){this.baseVoltageEsca = v}
+	
+	Set<EscaType> breakers = new HashSet<EscaType>();
+	protected void setBreakers(v){this.breakers = breakers}
+	
+	Set<EscaType> generators = new HashSet<EscaType>();
+	protected void setGenerators(Set<EscaType> generators) {this.generators = generators}
+		
 	String identifier;
-	EscaTypeBag loads;
-	EscaTypeBag shunts;
-	EscaTypeBag generators;
+	Set<EscaType> loads = new HashSet<EscaType>();
+	protected void setLoads(Set<EscaType> loads) {this.loads = loads}
 	
-	Resource voltageLevelRes;
+	Set<EscaType> shunts  = new HashSet<EscaType>()
+	protected void setShunts(Set<EscaType> shunts) {this.shunts = shunts}
+	
+	Set<EscaType> transformers = new HashSet<EscaType>();
+	protected void setTransformers(Set<EscaType> transformers) {this.transformers = transformers}
+	
+	EscaType voltageLevel;
+	protected void setVoltageLevel(EscaType item) {this.voltageLevel = item}
 	
 	Set<ConnectivityNode> connectivityNodes = new HashSet<ConnectivityNode>();
+	protected void setConnectivityNodes(Set<ConnectivityNodes> nodes) {this.connectivityNodes = nodes}
+	
+	
 	//ConnectivityNodes connectivityNodes = new ConnectivityNodes();
 	//Terminals terminals = new Terminals();
 	Set<Terminal> terminals = new HashSet<Terminal>();
-	
-	
+
+	def initialize(){
+
+		if (hasBeenInitialized) return
+		
+		connectivityNodes.each {
+			if (this.voltageLevel == null){
+				this.voltageLevel = it.getDirectLinks()?.find({it.isResourceType(EscaVocab.VOLTAGELEVEL_OBJECT)})
+			}
+		}
+		
+		voltageLevel?.getDirectLinks()?.each {
+			
+			if (it.isResourceType(EscaVocab.SUBSTATION_OBJECT)){
+				substation = it
+			}
+			else if(it.isResourceType(EscaVocab.BASEVOLTAGE_OBJECT)){
+				baseVoltage = it.getLiteralValue(EscaVocab.BASEVOLTAGE_NOMINALVOLTAGE).getDouble()
+				baseVoltageEsca = it
+			}
+		}
+		
+		def noPrint = [EscaVocab.CONNECTIVITYNODE_OBJECT, EscaVocab.TERMINAL_OBJECT, EscaVocab.BREAKER_OBJECT]
+		voltageLevel?.getRefersToMe()?.each{
+			if (it.isResourceType(EscaVocab.SHUNTCOMPENSATOR_OBJECT)){
+				shunts.add(it)
+			}
+			else if (it.isResourceType(EscaVocab.TRANSFORMERWINDING_OBJECT)){
+				transformers.add(it)
+			}
+			else if (it.isResourceType(EscaVocab.CONFORMLOAD_OBJECT)){
+				loads.add(it)
+			}
+			else if (it.isResourceType(EscaVocab.SYNCHRONOUSMACHINE_OBJECT)){
+				generators.add(it)
+			}
+			else if (it.isResourceType(EscaVocab.BREAKER_OBJECT)){
+				breakers.add(it)
+			}
+			else{
+				def printIt = true
+				noPrint.each{ other->
+					if (it.isResourceType(other)){
+						printIt = false
+					}
+				}
+				
+				//if (printIt) println "Unadded: "+ it
+			}
+		}
+		 
+//		connectivityNodes.each {
+//			it.getDirectLinks().each {
+//				printf("\t%s\n", it)
+//				if (it.isResourceType(EscaVocab.VOLTAGELEVEL_OBJECT)){
+//					this.voltageLevel = it
+//				}
+//				else if(it.isResourceType(EscaVocab.SYNCHRONOUSMACHINE_OBJECT)){
+//					this.generators.add(it)
+//				}
+//				else if(it.isResourceType(EscaVocab.BASEVOLTAGE_OBJECT)){
+//					println "Setting "+it
+//					this.baseVoltage = it.getLiteralValue(EscaVocab.BASEVOLTAGE_NOMINALVOLTAGE).getDouble()
+//					this.baseVoltageEsca = it
+//				}
+//			}
+////			it.getRefersToMe().each{
+////				if(it.isResourceType(EscaVocab.BASEVOLTAGE_OBJECT)){
+////					this.baseVoltage = it.getLiteralValue(EscaVocab.BASEVOLTAGE_NOMINALVOLTAGE).getDouble()
+////					this.baseVoltageEsca = it
+////				}
+////			}
+//		}	
+		hasBeenInitialized = true
+	}	
 }
