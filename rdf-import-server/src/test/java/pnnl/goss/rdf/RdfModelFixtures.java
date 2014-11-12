6ce19079b8b1adcabc1f66be112207a169ce5bad
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-11-11 22:48:32 -0800

    moved mapping of terminals to connectivity nodes into it's own function.

--- rdf-import-server/src/test/java/pnnl/goss/rdf/RdfModelFixtures.java
+++ rdf-import-server/src/test/java/pnnl/goss/rdf/RdfModelFixtures.java
@@ -24,144 +24,18 @@ public class RdfModelFixtures {
 		List<Resource> acLineSegment = addTypeToModel(model, EscaVocab.ACLINESEGMENT_OBJECT, "ACLine", 5);
 		List<Resource> loads = addTypeToModel(model, EscaVocab.LOADBREAKSWITCH_OBJECT, "L", 2);
 		List<Resource> generators = addTypeToModel(model, EscaVocab.SYNCHRONOUSMACHINE_OBJECT, "SM", 2);
-						
-		
 		List<Resource> connectivityNodes = addTypeToModel(model, EscaVocab.CONNECTIVITYNODE_OBJECT, "Conn", 35);
 	
-		Map<Integer, Integer> terminalToConnMap = new HashMap<>();
-
-		// SS1 - Column 1
-		terminalToConnMap.put(0, 0);
-		terminalToConnMap.put(1, 0);
-		
-		terminalToConnMap.put(2, 1);
-		terminalToConnMap.put(3, 1);  // Generator 1
-		terminalToConnMap.put(4, 1);
-		
-		terminalToConnMap.put(5, 2);
-		terminalToConnMap.put(6, 2);  // AC Line to CN 3 - 22
-		terminalToConnMap.put(7, 2);
-		
-		terminalToConnMap.put(8, 3);
-		terminalToConnMap.put(9, 3);
-
-		// SS1 - Column 2		
-		terminalToConnMap.put(10, 4);
-		terminalToConnMap.put(11, 4);
-		
-		terminalToConnMap.put(12, 5);
-		terminalToConnMap.put(13, 5);	// AC Line to CN 8 - 10
-		terminalToConnMap.put(14, 5);
-		
-		terminalToConnMap.put(15, 6);
-		terminalToConnMap.put(16, 6);	// AC Line to CN 7 - 28
-		terminalToConnMap.put(17, 6);
-		
-		terminalToConnMap.put(18, 7);
-		terminalToConnMap.put(19, 7);
-		
-		// SS2 - Column 1
-		terminalToConnMap.put(20, 8);
-		terminalToConnMap.put(21, 8);
-		
-		terminalToConnMap.put(22, 9);
-		terminalToConnMap.put(23, 9);  // AC Line to CN 8 - 10
-		terminalToConnMap.put(24, 9);
-		
-		terminalToConnMap.put(25, 10);
-		terminalToConnMap.put(26, 10);
-		
-		// SS2 - Column 2
-		terminalToConnMap.put(27, 11);		
-		terminalToConnMap.put(28, 11);
-		
-		terminalToConnMap.put(29, 12);
-		terminalToConnMap.put(30, 12);  // AC Line to CN 13 - 32
-		terminalToConnMap.put(31, 12);
-		
-		terminalToConnMap.put(32, 13);
-		terminalToConnMap.put(33, 13);
-		
-		// SS2 - Column 3
-		terminalToConnMap.put(34, 14);		
-		terminalToConnMap.put(35, 14);
-		
-		terminalToConnMap.put(36, 15);  
-		terminalToConnMap.put(37, 15);  // Generator 2
-		terminalToConnMap.put(38, 15);
-		
-		terminalToConnMap.put(39, 16);
-		terminalToConnMap.put(40, 16);
-		
-		// SS3 - Column 1
-		terminalToConnMap.put(41, 17);		
-		terminalToConnMap.put(42, 17);
-		
-		terminalToConnMap.put(43, 18);  
-		terminalToConnMap.put(44, 18);  // Load 1
-		terminalToConnMap.put(45, 18);
-		
-		terminalToConnMap.put(46, 19);
-		terminalToConnMap.put(47, 19);
-		
-		// SS3 - Column 2
-		terminalToConnMap.put(48, 20);
-		terminalToConnMap.put(49, 20);
-
-		terminalToConnMap.put(50, 21);
-		terminalToConnMap.put(51, 21);	// AC Line 3 - 22
-		terminalToConnMap.put(52, 21);
-		
-		terminalToConnMap.put(53, 22);
-		terminalToConnMap.put(54, 22);
-		
-		// SS3 - Column 3
-		terminalToConnMap.put(55, 23);
-		terminalToConnMap.put(56, 23); 
-		
-		terminalToConnMap.put(57, 24);		
-		terminalToConnMap.put(58, 24);  // AC Line 25 - 29 
-		terminalToConnMap.put(59, 24);
-		
-		terminalToConnMap.put(60, 25);
-		terminalToConnMap.put(61, 25);
-		
-		// SS4 - Column 1
-		terminalToConnMap.put(62, 26);
-		terminalToConnMap.put(63, 26);
-		
-		terminalToConnMap.put(64, 27);
-		terminalToConnMap.put(65, 27);  // AC Line 7 - 28
-		terminalToConnMap.put(66, 27);
-		
-		terminalToConnMap.put(67, 28);		
-		terminalToConnMap.put(68, 28);  // AC Line 25 - 29
-		terminalToConnMap.put(69, 28);
-
-		terminalToConnMap.put(70, 29);
-		terminalToConnMap.put(71, 29);		
-		
-		// SS4 - Column 2
-		terminalToConnMap.put(72, 30);	
-		terminalToConnMap.put(73, 30);
-		
-		terminalToConnMap.put(74, 31);	
-		terminalToConnMap.put(75, 31);  // AC Line 7 - 29
-		terminalToConnMap.put(76, 31);
-		
-		terminalToConnMap.put(77, 32);		
-		terminalToConnMap.put(78, 32);  // Load 2
-		terminalToConnMap.put(79, 32);
-		
-		terminalToConnMap.put(80, 33);
-		terminalToConnMap.put(81, 33);
-		
+		Map<Integer, Integer> terminalToConnMap = createTermToConnectivityNodeMap();
 		
 		createLinkProperty(terminalToConnMap, EscaVocab.TERMINAL_CONNECTIVITYNODE, terminals, connectivityNodes);
+		
+		//Map<Integer, Integer> 
 		
 		return model;
 	}
 	
+		
 	/**
 	 * Create links on elements in elementsToAddProperty that point to elements in propertyLinks.  The property
 	 * will be added according to the mapping passed where each key is an index in propertiesTo and the
@@ -202,5 +76,143 @@ public class RdfModelFixtures {
 		}
 		
 		return resources;
+	}
+	
+	/**
+	 * Creates a maping from terminal to connectivity node.  For this model there
+	 * are either 2 or 3 referencens depending upon whether connected to a circuit breaker
+	 * or not.
+	 * 
+	 * @return A map from terminal index to connectivity node index.
+	 */
+	private static Map<Integer, Integer> createTermToConnectivityNodeMap(){
+		Map<Integer, Integer> termToCnMap = new HashMap<>();
+
+		// SS1 - Column 1
+		termToCnMap.put(0, 0);
+		termToCnMap.put(1, 0);
+		
+		termToCnMap.put(2, 1);
+		termToCnMap.put(3, 1);  // Generator 1
+		termToCnMap.put(4, 1);
+		
+		termToCnMap.put(5, 2);
+		termToCnMap.put(6, 2);  // AC Line to CN 3 - 22
+		termToCnMap.put(7, 2);
+		
+		termToCnMap.put(8, 3);
+		termToCnMap.put(9, 3);
+
+		// SS1 - Column 2		
+		termToCnMap.put(10, 4);
+		termToCnMap.put(11, 4);
+		
+		termToCnMap.put(12, 5);
+		termToCnMap.put(13, 5);	// AC Line to CN 8 - 10
+		termToCnMap.put(14, 5);
+		
+		termToCnMap.put(15, 6);
+		termToCnMap.put(16, 6);	// AC Line to CN 7 - 28
+		termToCnMap.put(17, 6);
+		
+		termToCnMap.put(18, 7);
+		termToCnMap.put(19, 7);
+		
+		// SS2 - Column 1
+		termToCnMap.put(20, 8);
+		termToCnMap.put(21, 8);
+		
+		termToCnMap.put(22, 9);
+		termToCnMap.put(23, 9);  // AC Line to CN 8 - 10
+		termToCnMap.put(24, 9);
+		
+		termToCnMap.put(25, 10);
+		termToCnMap.put(26, 10);
+		
+		// SS2 - Column 2
+		termToCnMap.put(27, 11);		
+		termToCnMap.put(28, 11);
+		
+		termToCnMap.put(29, 12);
+		termToCnMap.put(30, 12);  // AC Line to CN 13 - 32
+		termToCnMap.put(31, 12);
+		
+		termToCnMap.put(32, 13);
+		termToCnMap.put(33, 13);
+		
+		// SS2 - Column 3
+		termToCnMap.put(34, 14);		
+		termToCnMap.put(35, 14);
+		
+		termToCnMap.put(36, 15);  
+		termToCnMap.put(37, 15);  // Generator 2
+		termToCnMap.put(38, 15);
+		
+		termToCnMap.put(39, 16);
+		termToCnMap.put(40, 16);
+		
+		// SS3 - Column 1
+		termToCnMap.put(41, 17);		
+		termToCnMap.put(42, 17);
+		
+		termToCnMap.put(43, 18);  
+		termToCnMap.put(44, 18);  // Load 1
+		termToCnMap.put(45, 18);
+		
+		termToCnMap.put(46, 19);
+		termToCnMap.put(47, 19);
+		
+		// SS3 - Column 2
+		termToCnMap.put(48, 20);
+		termToCnMap.put(49, 20);
+
+		termToCnMap.put(50, 21);
+		termToCnMap.put(51, 21);	// AC Line 3 - 22
+		termToCnMap.put(52, 21);
+		
+		termToCnMap.put(53, 22);
+		termToCnMap.put(54, 22);
+		
+		// SS3 - Column 3
+		termToCnMap.put(55, 23);
+		termToCnMap.put(56, 23); 
+		
+		termToCnMap.put(57, 24);		
+		termToCnMap.put(58, 24);  // AC Line 25 - 29 
+		termToCnMap.put(59, 24);
+		
+		termToCnMap.put(60, 25);
+		termToCnMap.put(61, 25);
+		
+		// SS4 - Column 1
+		termToCnMap.put(62, 26);
+		termToCnMap.put(63, 26);
+		
+		termToCnMap.put(64, 27);
+		termToCnMap.put(65, 27);  // AC Line 7 - 28
+		termToCnMap.put(66, 27);
+		
+		termToCnMap.put(67, 28);		
+		termToCnMap.put(68, 28);  // AC Line 25 - 29
+		termToCnMap.put(69, 28);
+
+		termToCnMap.put(70, 29);
+		termToCnMap.put(71, 29);		
+		
+		// SS4 - Column 2
+		termToCnMap.put(72, 30);	
+		termToCnMap.put(73, 30);
+		
+		termToCnMap.put(74, 31);	
+		termToCnMap.put(75, 31);  // AC Line 7 - 29
+		termToCnMap.put(76, 31);
+		
+		termToCnMap.put(77, 32);		
+		termToCnMap.put(78, 32);  // Load 2
+		termToCnMap.put(79, 32);
+		
+		termToCnMap.put(80, 33);
+		termToCnMap.put(81, 33);
+		return termToCnMap;
 	}
 }
