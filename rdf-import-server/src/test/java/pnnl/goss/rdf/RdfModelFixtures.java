725151d05b9f27d4edea7f0c8c53b1742decb0e2
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-11-11 22:36:58 -0800

    Added connections to cn and term from the 4 substation fixture in the paper.

--- rdf-import-server/src/test/java/pnnl/goss/rdf/RdfModelFixtures.java
+++ rdf-import-server/src/test/java/pnnl/goss/rdf/RdfModelFixtures.java
@@ -15,24 +15,147 @@ import com.sun.org.apache.xml.internal.security.keys.content.KeyValue;
 
 public class RdfModelFixtures {
 	
-	public static Model get10TerminalSystem(){
+	public static Model get4SubModel(){
 		Model model = ModelFactory.createDefaultModel();
 		
-		List<Resource> terminals = addTypeToModel(model, EscaVocab.TERMINAL_OBJECT, "Term", 10);
+		List<Resource> substations = addTypeToModel(model, EscaVocab.SUBSTATION_OBJECT, "SS",  4);
+		List<Resource> terminals = addTypeToModel(model, EscaVocab.TERMINAL_OBJECT, "Term", 82);
+		List<Resource> breakers = addTypeToModel(model, EscaVocab.BREAKER_OBJECT, "CB", 24);
+		List<Resource> acLineSegment = addTypeToModel(model, EscaVocab.ACLINESEGMENT_OBJECT, "ACLine", 5);
+		List<Resource> loads = addTypeToModel(model, EscaVocab.LOADBREAKSWITCH_OBJECT, "L", 2);
+		List<Resource> generators = addTypeToModel(model, EscaVocab.SYNCHRONOUSMACHINE_OBJECT, "SM", 2);
+						
 		
-		List<Resource> connectivityNodes = addTypeToModel(model, EscaVocab.CONNECTIVITYNODE_OBJECT, "Conn", 4);
+		List<Resource> connectivityNodes = addTypeToModel(model, EscaVocab.CONNECTIVITYNODE_OBJECT, "Conn", 35);
 	
 		Map<Integer, Integer> terminalToConnMap = new HashMap<>();
+
+		// SS1 - Column 1
 		terminalToConnMap.put(0, 0);
 		terminalToConnMap.put(1, 0);
+		
 		terminalToConnMap.put(2, 1);
-		terminalToConnMap.put(3, 1);
-		terminalToConnMap.put(4, 2);
+		terminalToConnMap.put(3, 1);  // Generator 1
+		terminalToConnMap.put(4, 1);
+		
 		terminalToConnMap.put(5, 2);
-		terminalToConnMap.put(6, 3);
-		terminalToConnMap.put(7, 3);
+		terminalToConnMap.put(6, 2);  // AC Line to CN 3 - 22
+		terminalToConnMap.put(7, 2);
+		
 		terminalToConnMap.put(8, 3);
 		terminalToConnMap.put(9, 3);
+
+		// SS1 - Column 2		
+		terminalToConnMap.put(10, 4);
+		terminalToConnMap.put(11, 4);
+		
+		terminalToConnMap.put(12, 5);
+		terminalToConnMap.put(13, 5);	// AC Line to CN 8 - 10
+		terminalToConnMap.put(14, 5);
+		
+		terminalToConnMap.put(15, 6);
+		terminalToConnMap.put(16, 6);	// AC Line to CN 7 - 28
+		terminalToConnMap.put(17, 6);
+		
+		terminalToConnMap.put(18, 7);
+		terminalToConnMap.put(19, 7);
+		
+		// SS2 - Column 1
+		terminalToConnMap.put(20, 8);
+		terminalToConnMap.put(21, 8);
+		
+		terminalToConnMap.put(22, 9);
+		terminalToConnMap.put(23, 9);  // AC Line to CN 8 - 10
+		terminalToConnMap.put(24, 9);
+		
+		terminalToConnMap.put(25, 10);
+		terminalToConnMap.put(26, 10);
+		
+		// SS2 - Column 2
+		terminalToConnMap.put(27, 11);		
+		terminalToConnMap.put(28, 11);
+		
+		terminalToConnMap.put(29, 12);
+		terminalToConnMap.put(30, 12);  // AC Line to CN 13 - 32
+		terminalToConnMap.put(31, 12);
+		
+		terminalToConnMap.put(32, 13);
+		terminalToConnMap.put(33, 13);
+		
+		// SS2 - Column 3
+		terminalToConnMap.put(34, 14);		
+		terminalToConnMap.put(35, 14);
+		
+		terminalToConnMap.put(36, 15);  
+		terminalToConnMap.put(37, 15);  // Generator 2
+		terminalToConnMap.put(38, 15);
+		
+		terminalToConnMap.put(39, 16);
+		terminalToConnMap.put(40, 16);
+		
+		// SS3 - Column 1
+		terminalToConnMap.put(41, 17);		
+		terminalToConnMap.put(42, 17);
+		
+		terminalToConnMap.put(43, 18);  
+		terminalToConnMap.put(44, 18);  // Load 1
+		terminalToConnMap.put(45, 18);
+		
+		terminalToConnMap.put(46, 19);
+		terminalToConnMap.put(47, 19);
+		
+		// SS3 - Column 2
+		terminalToConnMap.put(48, 20);
+		terminalToConnMap.put(49, 20);
+
+		terminalToConnMap.put(50, 21);
+		terminalToConnMap.put(51, 21);	// AC Line 3 - 22
+		terminalToConnMap.put(52, 21);
+		
+		terminalToConnMap.put(53, 22);
+		terminalToConnMap.put(54, 22);
+		
+		// SS3 - Column 3
+		terminalToConnMap.put(55, 23);
+		terminalToConnMap.put(56, 23); 
+		
+		terminalToConnMap.put(57, 24);		
+		terminalToConnMap.put(58, 24);  // AC Line 25 - 29 
+		terminalToConnMap.put(59, 24);
+		
+		terminalToConnMap.put(60, 25);
+		terminalToConnMap.put(61, 25);
+		
+		// SS4 - Column 1
+		terminalToConnMap.put(62, 26);
+		terminalToConnMap.put(63, 26);
+		
+		terminalToConnMap.put(64, 27);
+		terminalToConnMap.put(65, 27);  // AC Line 7 - 28
+		terminalToConnMap.put(66, 27);
+		
+		terminalToConnMap.put(67, 28);		
+		terminalToConnMap.put(68, 28);  // AC Line 25 - 29
+		terminalToConnMap.put(69, 28);
+
+		terminalToConnMap.put(70, 29);
+		terminalToConnMap.put(71, 29);		
+		
+		// SS4 - Column 2
+		terminalToConnMap.put(72, 30);	
+		terminalToConnMap.put(73, 30);
+		
+		terminalToConnMap.put(74, 31);	
+		terminalToConnMap.put(75, 31);  // AC Line 7 - 29
+		terminalToConnMap.put(76, 31);
+		
+		terminalToConnMap.put(77, 32);		
+		terminalToConnMap.put(78, 32);  // Load 2
+		terminalToConnMap.put(79, 32);
+		
+		terminalToConnMap.put(80, 33);
+		terminalToConnMap.put(81, 33);
+		
 		
 		createLinkProperty(terminalToConnMap, EscaVocab.TERMINAL_CONNECTIVITYNODE, terminals, connectivityNodes);
 		
--- rdf-import-server/src/test/java/pnnl/goss/specs/TestEscaTreeWindow.groovy
+++ rdf-import-server/src/test/java/pnnl/goss/specs/TestEscaTreeWindow.groovy
@@ -11,31 +11,36 @@ class TestEscaTreeWindow extends Specification {
 	
 	def "constructing object with 10 Terminal system"() {
 		when:
-			EscaTreeWindow window = new EscaTreeWindow(RdfModelFixtures.get10TerminalSystem())
+			EscaTreeWindow window = new EscaTreeWindow(RdfModelFixtures.get4SubModel())
 		then:
 			assert window.getEscaTypeMap() != null
 			EscaTypes escaTypes = window.getEscaTypeMap()
-			assert escaTypes.getByResourceType(EscaVocab.TERMINAL_OBJECT).size() == 10
-			assert escaTypes.getByResourceType(EscaVocab.CONNECTIVITYNODE_OBJECT).size() == 4
 			
-			for(int i=0; i<escaTypes.getByResourceType(EscaVocab.CONNECTIVITYNODE_OBJECT).size(); i++){
-				String key = "Conn"+i
-				EscaType esca = escaTypes.get(key)
-				assert esca != null
-				if (i > 2){
-					assert esca.getRefersToMe().size() == 4
-				}
-				else{
-					assert esca.getRefersToMe().size() == 2
-				}
-			}
+			assert escaTypes.getByResourceType(EscaVocab.ACLINESEGMENT_OBJECT).size() == 5
+			assert escaTypes.getByResourceType(EscaVocab.SYNCHRONOUSMACHINE_OBJECT).size() == 2
+			assert escaTypes.getByResourceType(EscaVocab.LOADBREAKSWITCH_OBJECT).size() == 2
+			assert escaTypes.getByResourceType(EscaVocab.BREAKER_OBJECT).size() == 24
+			assert escaTypes.getByResourceType(EscaVocab.TERMINAL_OBJECT).size() == 82
+			assert escaTypes.getByResourceType(EscaVocab.CONNECTIVITYNODE_OBJECT).size() == 35
 			
-			for(int i=0; i<escaTypes.getByResourceType(EscaVocab.TERMINAL_OBJECT).size(); i++){
-				String key = "Term"+i
-				EscaType esca = escaTypes.get(key)
-				assert esca != null
-				esca.getDirectLinks().size() == 1
-			}
+//			for(int i=0; i<escaTypes.getByResourceType(EscaVocab.CONNECTIVITYNODE_OBJECT).size(); i++){
+//				String key = "Conn"+i
+//				EscaType esca = escaTypes.get(key)
+//				assert esca != null
+//				if (i > 2){
+//					assert esca.getRefersToMe().size() == 4
+//				}
+//				else{
+//					assert esca.getRefersToMe().size() == 2
+//				}
+//			}
+//			
+//			for(int i=0; i<escaTypes.getByResourceType(EscaVocab.TERMINAL_OBJECT).size(); i++){
+//				String key = "Term"+i
+//				EscaType esca = escaTypes.get(key)
+//				assert esca != null
+//				esca.getDirectLinks().size() == 1
+//			}
 				
 	}
 
